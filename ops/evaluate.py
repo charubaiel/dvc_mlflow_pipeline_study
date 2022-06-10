b833f0778978e770e28caaf209028614c666eacc
Charubaiel <alexandrin.a@mail.ru>
2022-06-10 18:33:12 +0300

    upd due mlops update | add examples| return inits

--- ops/evaluate.py
+++ ops/evaluate.py
@@ -1,13 +1,19 @@
 from __init__ import *
 from utils import *
+from mlflow.models.signature import infer_signature
+
+
+
+mlflow.set_tracking_uri("http://localhost:8118")
+# mlflow.autolog(log_input_examples=True)
 
 
 
 def evaluate_run(params,path_to_df='data/eval_df.csv'):
 
-    mlflow.set_tracking_uri("http://localhost:5000")
 
-    with mlflow.start_run(run_name='exp1'):
+    with mlflow.start_run(run_name='docker_server_test'):
+
 
         df = pd.read_csv(path_to_df).set_index('tconst')
         data = df.drop('averageRating',axis=1)
@@ -20,9 +26,11 @@ def evaluate_run(params,path_to_df='data/eval_df.csv'):
         lr = linear_model.ElasticNet(alpha=params['alpha'])
 
         lr.fit(x,y)
+        singaturka = infer_signature(xv,yv)
         mlflow.sklearn.log_model(lr,
                                 params['models_path'],
-                                registered_model_name="elastic-model")
+                                registered_model_name="elastic-model",
+                                signature=singaturka)
 
         mae_clip = np.mean(np.abs(np.round(lr.predict(xv),0) - yv.round(0)))
         mae = np.mean(np.abs(lr.predict(xv) - yv))
--- utils.py
+++ utils.py
@@ -12,14 +12,12 @@ import joblib
 import sys
 import os
 
-
 sys.path.append(os.getcwd())
-
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 
-with open('data/config.yml') as  w:
+with open('ops/config.yml') as  w:
     params = yaml.safe_load(w)
     params_download = params['stage']['download']['params']
     params_eval = params['stage']['evaluate']['params']
